5aa604b087f997aaa7757dda40798d4da6c692f3
Dmitriy <konovalovdima2009@yandex.ru>
2018-06-05 22:52:15 +0300

    repair easy_install

--- help_files/easy_install.bat
+++ help_files/easy_install.bat
@@ -2,5 +2,10 @@ cd ..
 python -m venv env
 call env\Scripts\activate
 pip install -r requirements.txt
+cd data_science_app
+md migrations
+cd migrations
+REM. > __init__.py
+cd ..\..
 python manage.py makemigrations
 python manage.py migrate
